1cce97838c80685ab2896ed0e9523f05e7d73f05
Myltiane <myltiane.aux@gmail.com>
2022-09-08 13:50:28 -0300

    feat: adicionando prisma e .env no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { PrismaModule } from './prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
