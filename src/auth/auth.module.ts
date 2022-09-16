052aafa47d1e45434d983eac6bbb7d8e9dc1ad7b
Myltiane <myltiane.aux@gmail.com>
2022-09-16 10:35:52 -0300

    feat: adicionando o jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
@@ -7,7 +8,15 @@ import { AuthService } from './auth.service';
 @Module({
    imports: [
       PrismaModule,
-      UserModule
+      UserModule,
+      JwtModule.registerAsync({
+         useFactory: () => ({
+            secret: process.env.JWT_SECRET,
+            signOptions: {
+               expiresIn: process.env.JWT_EXPIRE
+            },
+         })
+      })
    ],
    controllers: [
       AuthController
